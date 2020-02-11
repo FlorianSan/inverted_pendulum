52d50cbdd6b4f5058134153fe429be95d4fe18c5
flo12 <fl.sansou@gmail.com>
2020-02-11 21:10:48 +0100

    Sim open loop

--- dyn.m
+++ dyn.m
@@ -1,21 +1,23 @@
-function Xd = dyn(X,U,P,Sv)
-    Xd = zeros(1,Sv.TOTAL_SV);
+function Xd = dyn(t,X,U,P,Sv)
+    Xd = zeros(Sv.TOTAL_SV,1);
     th = X(Sv.itheta);
     thd = X(Sv.ithetad);
-    sth = math.sin(th);
-    cth = math.cos(th);
+    sth = sin(th);
+    cth = cos(th);
     % saturate input
-    tau = clip(U(Sv.it), -P.tsat, P.tsat);
+    tau = U(Sv.it);
+    tau(tau>P.tsat) = P.tsat ;
+    tau(tau<-P.tsat) = -P.tsat ;
     % upper rows are found directly in state vector
-    Xd(ix)  = X(Sv.ixd);
-    Xd(itheta) = thd;
+    Xd(Sv.ix)  = X(Sv.ixd);
+    Xd(Sv.itheta) = thd;
     % compute the lower rows
     a = P.h*cth;
     i = 1./(a^2-P.bc);
-    d = P.mb*P.g*P.L*sth - tau;
+    d = P.mb*P.gGravity*P.L*sth - tau;
     e = P.mb*P.R*P.L*thd^2*sth + tau;
     
-    Xd(s_xd)  = -P.R*i*(a*d - P.b*e);
-    Xd(s_thetad) = i*(-P.c*d + a*e);
+    Xd(Sv.ixd)  = -P.R*i*(a*d - P.b*e);
+    Xd(Sv.ithetad) = i*(-P.c*d + a*e);
 end
 
--- main.m
+++ main.m
@@ -7,7 +7,18 @@ P = Param;
 Sv = SV;
 U = 0;
 
-X0 = 0;
-time = (0:0.01:10);
-X = ode45(dyn, time, X0, , args=(U, P ))
-U = zeros((length(time), 1));
+X0 = [0 0.01 0 0];
+tspan = [0 10];
+%ode45(@(t,y)Reactor_simple_ODE(t,y,a1,a2,a3),span,init,options);
+test = dyn(0,X0,U,P,Sv);
+[t,X] = ode45(@(t,X)dyn(t,X,U,P,Sv),tspan,X0);
+%U = zeros((length(time), 1));
+
+%affichage de x
+plot(t,X(:,Sv.ix));
+%affichage de xdot
+plot(t,X(:,Sv.ixd));
+%affichage de theta
+plot(t,X(:,Sv.itheta));
+%affichage de thetadot
+plot(t,X(:,Sv.ithetad));
