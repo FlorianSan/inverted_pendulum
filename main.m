2ee70427882d72349a3dd6c2f5a4ebe398e85297
flo12 <fl.sansou@gmail.com>
2020-02-11 21:27:27 +0100

    feedback

--- main.m
+++ main.m
@@ -7,12 +7,11 @@ P = Param;
 Sv = SV;
 U = 0;
 
+%Open Loop
 X0 = [0 0.01 0 0];
 tspan = [0 10];
-%ode45(@(t,y)Reactor_simple_ODE(t,y,a1,a2,a3),span,init,options);
-test = dyn(0,X0,U,P,Sv);
 [t,X] = ode45(@(t,X)dyn(t,X,U,P,Sv),tspan,X0);
-%U = zeros((length(time), 1));
+U = zeros(length(t),1);
 
 %affichage de x
 plot(t,X(:,Sv.ix));
@@ -22,3 +21,10 @@ plot(t,X(:,Sv.ixd));
 plot(t,X(:,Sv.itheta));
 %affichage de thetadot
 plot(t,X(:,Sv.ithetad));
+
+%feedback
+K = 10;
+[t,X] = ode45(@(t,X)feedback(t,X,K,P,Sv),tspan,X0);
+
+
+
