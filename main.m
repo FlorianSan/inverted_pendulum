f628944ffb49561ebd149dfe10e8a3425333541c
flo12 <fl.sansou@gmail.com>
2020-02-12 09:03:06 +0100

    mise en place lqr et placement de pole

--- main.m
+++ main.m
@@ -23,8 +23,25 @@ plot(t,X(:,Sv.itheta));
 plot(t,X(:,Sv.ithetad));
 
 %feedback
-K = 10;
+%[t,X] = ode45(@(t,X)feedback(t,X,K,P,Sv),tspan,X0);
+
+
+[A, B] = num_jacobian([0 0 0 0], 0, P,Sv)
+eig(A)
+ctrb(A, B)
+
+
+%pole
+poles = [-2 -3 -4 -5];
+K = place(A, B, poles)
+eig(A-B*K)
 [t,X] = ode45(@(t,X)feedback(t,X,K,P,Sv),tspan,X0);
 
+%lqr
+Q = diag([10, 1, 1, 1]);
+R = diag(2);
+[K_lqr,S,E] = lqr(A,B,Q,R);
+eig(A-B*K_lqr)
+[t,X] = ode45(@(t,X)feedback(t,X,K_lqr,P,Sv),tspan,X0);
 
 
